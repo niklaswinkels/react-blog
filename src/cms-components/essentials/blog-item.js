01451314e3255598d2abdf4a63be76eac0785856
Lef Karamoulas <lef.karamoulas@bloomreach.com>
2018-06-13 15:56:23 +0200

    Make whole blog cards clickable to detail page

--- src/cms-components/essentials/blog-item.js
+++ src/cms-components/essentials/blog-item.js
@@ -5,12 +5,90 @@ import {Col} from "reactstrap";
 import "../../components/Card/style.css";
 
 export default class BlogItem extends React.Component {
-  renderLink (content) {
+  renderBlogLink (content,author,text) {
     if (content._links && content._links.site && content._links.site.href) {
       if (content._links.site.type === 'internal') {
-        return (<Link to={content._links.site.href}>{content.title}</Link>);
+        return (
+          <Link to={content._links.site.href}>
+            <div className="post-img">
+              <img
+                width="200"
+                height="80"
+                src={content.externalUrl}
+                className="attachment-aerial_thumbnail size-aerial_thumbnail wp-post-image"
+                alt="Supporting the Open Source Software that makes us Great"
+                scale="0"
+              />
+            </div>
+            <div className="post-content">
+              <div className="post-date">{parseDate(content.publicationDate)}</div>
+              <h2 className="entry-title">
+                {content.title}
+              </h2>
+              <div className="post-excerpt">
+                <p>{text}</p>
+              </div>
+              <div className="entry-meta clear">
+                <div className="author-gravatar">
+                  <img
+                    alt=""
+                    src={author.image.original._links.site.href}
+                    className="avatar avatar-40 photo"
+                    height="40"
+                    width="40"
+                    originals="40"
+                    scale="2"
+                  />
+                </div>
+                <div className="entry-author-content">
+                  <div className="author-name">{author.displayName}</div>
+                  <div className="post-meta-info"> 6 min read on BloomReach</div>
+                </div>
+              </div>
+            </div>
+          </Link>
+        );
       } else {
-        return (<a href={content._links.site.href}>{content.title}</a>)
+        return (
+          <a href={content._links.site.href}>
+            <div className="post-img">
+              <img
+                width="200"
+                height="80"
+                src={content.externalUrl}
+                className="attachment-aerial_thumbnail size-aerial_thumbnail wp-post-image"
+                alt="Supporting the Open Source Software that makes us Great"
+                scale="0"
+              />
+            </div>
+            <div className="post-content">
+              <div className="post-date">{parseDate(content.publicationDate)}</div>
+              <h2 className="entry-title">
+                {content.title}
+              </h2>
+              <div className="post-excerpt">
+                <p>{text}</p>
+              </div>
+              <div className="entry-meta clear">
+                <div className="author-gravatar">
+                  <img
+                    alt=""
+                    src={author.image.original._links.site.href}
+                    className="avatar avatar-40 photo"
+                    height="40"
+                    width="40"
+                    originals="40"
+                    scale="2"
+                  />
+                </div>
+                <div className="entry-author-content">
+                  <div className="author-name">{author.displayName}</div>
+                  <div className="post-meta-info"> 6 min read on BloomReach</div>
+                </div>
+              </div>
+            </div>
+          </a>
+        )
       }
     }
     return null;
@@ -22,63 +100,12 @@ export default class BlogItem extends React.Component {
 
     var author = this.props.pageModel.content[content.authors[0]["$ref"].split("/")[2]];
     var text = content.introduction;
-
     return (
       <Col md="6" lg="4">
-      <div className="post has-edit-button">
-        { manageContentButton && manageContentButton }
-
-        <div className="post-img">
-            <img
-              width="200"
-              height="80"
-              src={content.externalUrl}
-              className="attachment-aerial_thumbnail size-aerial_thumbnail wp-post-image"
-              alt="Supporting the Open Source Software that makes us Great"
-              scale="0"
-            />
+        <div className="post has-edit-button">
+          {manageContentButton && manageContentButton}
+          {this.renderBlogLink(content,author,text)}
         </div>
-        <div className="post-content">
-          <div className="post-date">{parseDate(content.publicationDate)}</div>
-          <h2 className="entry-title">
-              {content.title}
-          </h2>
-          <div className="post-excerpt">
-            <p>{text}</p>
-          </div>
-          <div className="entry-meta clear">
-            <div className="author-gravatar">
-              <img
-                alt=""
-                src={author.image.original._links.site.href}
-                className="avatar avatar-40 photo"
-                height="40"
-                width="40"
-                originals="40"
-                scale="2"
-              />
-            </div>
-            <div className="entry-author-content">
-              <div className="author-name">{author.displayName}</div>
-              <div className="post-meta-info"> 6 min read on BloomReach</div>
-            </div>
-          </div>
-        </div>
-        {/*<h2 className="blog-post-title">*/}
-          {/*{ this.renderLink(content) }*/}
-        {/*</h2>*/}
-        {/*<p className="blog-post-meta">*/}
-          {/*{ content.publicationDate &&*/}
-            {/*<span className="blog-post-date">{parseDate(content.publicationDate)}</span>*/}
-          {/*}*/}
-          {/*{ content.author &&*/}
-            {/*<span className="author"><a href="#pagination">{content.author}</a></span>*/}
-          {/*}*/}
-        {/*</p>*/}
-        {/*{ content.introduction &&*/}
-          {/*<p>{content.introduction}</p>*/}
-        {/*}*/}
-      </div>
       </Col>
     );
   }
